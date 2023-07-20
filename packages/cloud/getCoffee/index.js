0f32199bc67cf409c597cfc29a6024818d4036a2
cityplanner.biz <74056468+piergiorgio-roveda@users.noreply.github.com>
2023-07-20 12:15:33 +0300

    completed tutorial1

--- packages/cloud/getCoffee/index.js
+++ packages/cloud/getCoffee/index.js
@@ -14,7 +14,7 @@ async function main() {
         const inventory = await client.db("do-maps").collection("available-maps").find().toArray();
         console.log(inventory);
         return {
-            "body": inventory
+            "body_x": inventory
         };
     } catch (e) {
         console.error(e);
